c70c8a7a1e008bb159343ad20dbe2cbfb81788c2
User <User@DESKTOP-GNESREK>
2019-06-12 15:25:11 +0200

    test

---
+++ src/model/Abschnitt.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Abschnitt {
+
+	public void addWord(String format) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+	
+
+}
---
+++ src/model/Artikel.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Artikel {
+	
+	public enum style {Normal,Italic,BoldItalic,Bold,Hochgestellt}
+
+	public void expand(Abschnitt abschnitt) {
+		// TODO Auto-generated method stub
+		
+	};
+
+}
