7e4d09152339db102e035181542cb6138ef53a01
User <User@DESKTOP-GNESREK>
2019-06-24 09:43:01 +0200

    files deleted

--- src/controller/PDFController.java
+++ src/controller/PDFController.java
@@ -1,15 +1,5 @@
 package controller;
 import java.io.IOException;
-import java.util.ArrayList;
-
-import model.Abschnitt;
-import model.Artikel;
-import model.Artikel.style;
-import model.FontFilter;
-import model.Main;
-import model.Session;
-import model.Start;
-import model.Word;
 
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.FilteredTextRenderListener;
@@ -17,8 +7,10 @@ import com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import com.itextpdf.text.pdf.parser.RenderFilter;
 import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
-import com.itextpdf.text.pdf.parser.TextRenderInfo;
-import com.itextpdf.text.pdf.parser.Vector;
+
+import model.FontFilter;
+import model.Start;
+import model.Word;
 
 
 public class PDFController {	
